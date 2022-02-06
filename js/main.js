e518fc26592bab064c4f940a16ffda799355b253
charly92cjo <charlyotero92@gmail.com>
2022-02-06 15:02:32 -0300

    Pagina giyo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var map = L.map("map").setView([-34.9768211, -67.6886061], 19);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([-34.9769211, -67.6886261])
+  .addTo(map)
+  .bindPopup("Estudio Jurídico.<br>Gisela Durán.<br>Zamenoff 52")
+  .openPopup();
